083311dc1c908ec906c88e8d087b28bd63d1408e
kayque lima <kayqueferreira185@gmail.com>
2026-05-11 21:26:06 -0300

    feat: create contacts dynamically using DOM

--- script.js
+++ script.js
@@ -6,3 +6,43 @@ botaoform.addEventListener('click', () =>{
     formulario.classList.toggle('hidden');
 
 });
+
+const botaoSave = document.querySelector ('.save-button');
+const listacontatos = document.querySelector('.lista-contatos');
+const inputName = document.querySelector('#nome');
+const inputEmail = document.querySelector('#email');
+const inputTel = document.querySelector('#telefone');
+
+botaoSave.addEventListener('click', () =>{
+
+    const nome = inputName.value;
+    const email = inputEmail.value;
+    const numeroP = inputTel.value;
+    const img = document.createElement('img');
+    img.alt = 'avatar padrão';
+    img.src = 'imagens/imagem_avatar.png';
+    const h2 = document.createElement ('h2');
+    h2.textContent = nome;
+    const emailP = document.createElement ('p');
+    emailP.textContent = email;
+
+    const telefoneP = document.createElement ('p');
+    telefoneP.textContent = numeroP;
+
+    const divCreate = document.createElement ('div');
+    divCreate.classList.add('info');
+    img.classList.add('imagem');
+
+    divCreate.appendChild(h2);
+    divCreate.appendChild(telefoneP);
+    divCreate.appendChild(emailP);
+
+    const li = document.createElement('li');
+    li.classList.add('contato')
+    li.appendChild(img);
+    li.appendChild(divCreate);
+    
+    
+    listacontatos.appendChild(li);
+
+});
